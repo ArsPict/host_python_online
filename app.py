4802e437820753d0c9e341d84f29679db6d0a748
ArsPict <arsenii.picturesafe@gmail.com>
2024-07-08 11:55:53 +0200

    me

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import time
 from datetime import datetime
+import schedule
 
 
 def log_message():
@@ -9,6 +10,19 @@ def log_message():
     print(message)
 
 
-while True:
-    log_message()
+def stop_running():
+    global keep_running
+    keep_running = False
+    print("Stopping the scheduler.")
+
+
+schedule.every(10).seconds.do(log_message)
+schedule.every(1).minute.do(stop_running)
+
+keep_running = True
+
+while keep_running:
+    schedule.run_pending()
     time.sleep(2)
+
+print("program finished")
